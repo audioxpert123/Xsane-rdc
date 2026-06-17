b17ff9512cdc3808b476d454fc6a836c5ab975eb
audioxpert123 <audioxpert123@gmail.com>
2026-06-17 12:40:36 +0530

    Add structures for doors and walls
    Added an array of structures representing various doors and walls with their names, categories, and image paths.

---
+++ data.js
@@ -0,0 +1,51 @@
+const structures = [
+
+{
+name:"Wooden Door",
+category:"doors",
+image:"images/wooden-door.webp"
+},
+
+{
+name:"Sheet Metal Door",
+category:"doors",
+image:"images/sheet-metal-door.webp"
+},
+
+{
+name:"Garage Door",
+category:"doors",
+image:"images/garage-door.webp"
+},
+
+{
+name:"Armored Door",
+category:"doors",
+image:"images/armored-door.webp"
+},
+
+{
+name:"Wooden Wall",
+category:"walls",
+image:"images/wooden-wall.webp"
+},
+
+{
+name:"Stone Wall",
+category:"walls",
+image:"images/stone-wall.webp"
+},
+
+{
+name:"Sheet Metal Wall",
+category:"walls",
+image:"images/sheet-metal-wall.webp"
+},
+
+{
+name:"Armored Wall",
+category:"walls",
+image:"images/armored-wall.webp"
+}
+
+];
