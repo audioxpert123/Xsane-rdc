a3428183021f6408fbfd4301b9529e68d8c985aa
audioxpert123 <audioxpert123@gmail.com>
2026-06-18 19:10:10 +0530

    Fix formatting of Armored Door entry in structures

--- data.js
+++ data.js
@@ -1,4 +1,8 @@
+const structures = [
+
 {name:"Wooden Door",category:"doors",image:"images/wooden-door.png"},
 {name:"Sheet Metal Door",category:"doors",image:"images/sheet-metal-door.webp"},
 {name:"Garage Door",category:"doors",image:"images/garage-door.webp"},
-{name:"Armored Door",category:"doors",image:"images/armored-door.webp"},
+{name:"Armored Door",category:"doors",image:"images/armored-door.webp"}
+
+];
