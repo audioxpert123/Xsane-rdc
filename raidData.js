ae985d1013f583764a74aec533f5fbc3ac82c0d7
audioxpert123 <audioxpert123@gmail.com>
2026-06-18 17:53:41 +0530

    Add raid data for various door types

---
+++ raidData.js
@@ -0,0 +1,25 @@
+const raidData = {
+  "Wooden Door": {
+    hp: 200,
+    raidItem: "Molotov",
+    quantity: 3
+  },
+
+  "Sheet Metal Door": {
+    hp: 250,
+    raidItem: "Satchel Charge",
+    quantity: 4
+  },
+
+  "Garage Door": {
+    hp: 600,
+    raidItem: "Rocket",
+    quantity: 9
+  },
+
+  "Armored Door": {
+    hp: 800,
+    raidItem: "Rocket",
+    quantity: 12
+  }
+};
