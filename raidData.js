0d1b08593dcf74791ea083d22af3f0f4454e3ec0
audioxpert123 <audioxpert123@gmail.com>
2026-06-20 12:35:28 +0530

    Update raidData.js

--- raidData.js
+++ raidData.js
@@ -1,25 +1,43 @@
 const raidData = {
+
   "Wooden Door": {
     hp: 200,
-    image: "molotov.webp",
-    quantity: 3
+    bestMethod: [
+      {
+        image: "molotov.webp",
+        quantity: 3
+      }
+    ]
   },
 
   "Sheet Metal Door": {
     hp: 250,
-    image: "satchel.webp",
-    quantity: 4
+    bestMethod: [
+      {
+        image: "satchel.webp",
+        quantity: 4
+      }
+    ]
   },
 
   "Garage Door": {
     hp: 600,
-    image: "rocket.webp",
-    quantity: 9
+    bestMethod: [
+      {
+        image: "rocket.webp",
+        quantity: 9
+      }
+    ]
   },
 
   "Armored Door": {
     hp: 800,
-    image: "rocket.webp",
-    quantity: 12
+    bestMethod: [
+      {
+        image: "rocket.webp",
+        quantity: 12
+      }
+    ]
   }
+
 };
